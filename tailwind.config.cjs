addafc5b5179a4f2d53ca8515e014ebcf3f998b8
Sacha Rodier <rodier@neoskop.de>
2022-05-27 01:55:32 +0200

    adding dependencies

---
+++ tailwind.config.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+  theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem'
+      }
+    },
+    extend: {},
+  },
+  plugins: [],
+}
