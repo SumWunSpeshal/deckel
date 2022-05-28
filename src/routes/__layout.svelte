e2f60e184e1018d9c2d718cce57efe7c9040acae
Sacha Rodier <rodier@neoskop.de>
2022-05-28 21:14:52 +0200

    removing styles

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -104,15 +104,3 @@
     Nesciunt deleniti earum totam accusantium alias fuga provident corporis quis
   </div>
 </footer>
-
-<style>
-  main {
-    flex: 1;
-    display: flex;
-    flex-direction: column;
-    width: 100%;
-    max-width: 1024px;
-    margin: 0 auto;
-    box-sizing: border-box;
-  }
-</style>
