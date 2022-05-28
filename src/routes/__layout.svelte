b112b30fe6c7f24866e8c63aa87b4aa554f96612
Sacha Rodier <rodier@neoskop.de>
2022-05-28 14:45:34 +0200

    first implementation of draggable drawer

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,45 +1,109 @@
 <script>
-	import Header from '$lib/header/Header.svelte';
-	import '../app.css';
+  import { page } from '$app/stores'
+  import { onMount, afterUpdate } from 'svelte'
+
+  import '../app.css'
+
+  let nav
+  let reserveBottom = 0
+  let locked = true
+  let touchY = null
+
+  onMount(() => {
+    reserveBottom = nav.getBoundingClientRect().height
+  })
+
+  function onTouchStart({ touches: [touch] }) {
+    touchY = touch.clientY
+  }
+
+  function onTouchMove({ touches: [touch] }) {
+    locked = touch.clientY > touchY
+  }
+
+  function onTouchEnd() {
+    touchY = null
+  }
 </script>
 
-<Header />
-
-<main>
-	<slot />
+<main style="padding-bottom: {reserveBottom}px;">
+  <slot />
 </main>
+<footer
+  class="fixed overflow-y-auto text-center inset-0 snap-y snap-mandatory z-10"
+  style="pointer-events: {locked ? 'none' : 'all'}"
+>
+  <div class="flex flex-col justify-end h-full snap-end">
+    <nav
+      class="flex justify-center p-4 border border-red-600 bg-white pointer-events-auto"
+      bind:this={nav}
+      on:touchstart={onTouchStart}
+      on:touchmove={onTouchMove}
+      on:touchend={onTouchEnd}
+    >
+      <ul class="flex">
+        <li class:active={$page.url.pathname === '/'}>
+          <a sveltekit:prefetch href="/">Home</a>
+        </li>
+        <li class:active={$page.url.pathname === '/about'}>
+          <a sveltekit:prefetch href="/about">About</a>
+        </li>
+        <li class:active={$page.url.pathname === '/todos'}>
+          <a sveltekit:prefetch href="/todos">Todos</a>
+        </li>
+      </ul>
+    </nav>
+  </div>
 
-<footer>
-	<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
+  <div class="snap-start bg-white">
+    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odio, est
+    explicabo perspiciatis minima velit fugit consequuntur voluptate odit!
+    Nesciunt deleniti earum totam accusantium alias fuga provident corporis quis
+    ut ex! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odio, est
+    explicabo perspiciatis minima velit fugit consequuntur voluptate odit!
+    Nesciunt deleniti earum totam accusantium alias fuga provident corporis quis
+    ut ex! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odio, est
+    explicabo perspiciatis minima velit fugit consequuntur voluptate odit!
+    Nesciunt deleniti earum totam accusantium alias fuga provident corporis quis
+    ut ex! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odio, est
+    explicabo perspiciatis minima velit fugit consequuntur voluptate odit!
+    Nesciunt deleniti earum totam accusantium alias fuga provident corporis quis
+    ut ex! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odio, est
+    explicabo perspiciatis minima velit fugit consequuntur voluptate odit!
+    Nesciunt deleniti earum totam accusantium alias fuga provident corporis quis
+    ut ex! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odio, est
+    explicabo perspiciatis minima velit fugit consequuntur voluptate odit!
+    Nesciunt deleniti earum totam accusantium alias fuga provident corporis quis
+    ut ex! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odio, est
+    explicabo perspiciatis minima velit fugit consequuntur voluptate odit!
+    Nesciunt deleniti earum totam accusantium alias fuga provident corporis quis
+    ut ex! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odio, est
+    explicabo perspiciatis minima velit fugit consequuntur voluptate odit!
+    Nesciunt deleniti earum totam accusantium alias fuga provident corporis quis
+    ut ex! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odio, est
+    explicabo perspiciatis minima velit fugit consequuntur voluptate odit!
+    Nesciunt deleniti earum totam accusantium alias fuga provident corporis quis
+    ut ex! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odio, est
+    explicabo perspiciatis minima velit fugit consequuntur voluptate odit!
+    Nesciunt deleniti earum totam accusantium alias fuga provident corporis quis
+    ut ex!
+
+    <!-- Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odio, est explicabo perspiciatis minima velit fugit consequuntur voluptate odit! Nesciunt deleniti earum totam accusantium alias fuga provident corporis quis ut ex!	
+		Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odio, est explicabo perspiciatis minima velit fugit consequuntur voluptate odit! Nesciunt deleniti earum totam accusantium alias fuga provident corporis quis ut ex!	
+		Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odio, est explicabo perspiciatis minima velit fugit consequuntur voluptate odit! Nesciunt deleniti earum totam accusantium alias fuga provident corporis quis ut ex!	
+		Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odio, est explicabo perspiciatis minima velit fugit consequuntur voluptate odit! Nesciunt deleniti earum totam accusantium alias fuga prov -->
+  </div>
 </footer>
 
 <style>
-	main {
-		flex: 1;
-		display: flex;
-		flex-direction: column;
-		padding: 1rem;
-		width: 100%;
-		max-width: 1024px;
-		margin: 0 auto;
-		box-sizing: border-box;
-	}
-
-	footer {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		padding: 40px;
-	}
-
-	footer a {
-		font-weight: bold;
-	}
-
-	@media (min-width: 480px) {
-		footer {
-			padding: 40px 0;
-		}
-	}
+  main {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    padding: 1rem;
+    width: 100%;
+    max-width: 1024px;
+    margin: 0 auto;
+    box-sizing: border-box;
+  }
 </style>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,7 +7,21 @@ module.exports = {
         DEFAULT: '1rem'
       }
     },
-    extend: {},
+    extend: {
+      inset: ({ theme }) => ({
+        auto: 'auto',
+        ...theme('spacing'),
+        '1/2': '50%',
+        '1/3': '33.333333%',
+        '2/3': '66.666667%',
+        '1/4': '25%',
+        '2/4': '50%',
+        '3/4': '75%',
+        full: '100%',
+        'screen-w': '100vw',
+        'screen-h': '100vh'
+      })
+    }
   },
-  plugins: [],
+  plugins: []
 }
